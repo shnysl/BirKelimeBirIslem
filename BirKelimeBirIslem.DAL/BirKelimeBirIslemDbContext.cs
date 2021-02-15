39efea581b72e4d79e0e6bb95c3218dcab5b34ca
Sahin Uysal <sahin.uysal@outlook.com>
2021-02-15 23:44:12 +0300

    Created

---
+++ BirKelimeBirIslem.DAL/BirKelimeBirIslemDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BirKelimeBirIslem.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirKelimeBirIslem.DAL
+{
+    class BirKelimeBirIslemDbContext:DbContext
+    {
+        public BirKelimeBirIslemDbContext() : base("Server=DESKTOP-Q21DV4G\\DENEME; Database=BirKelimeBirIslem; uid=sa; pwd=123")
+        {
+
+        }
+
+        public DbSet<KelimeModel> Kelimeler { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new KelimeMapping());
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
